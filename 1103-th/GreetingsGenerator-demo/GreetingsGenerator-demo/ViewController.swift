d80b306b3946774f8e440cc62db3c0094e086cae
Nicholas Swift <nicholas.w.swift@gmail.com>
2016-11-11 16:03:19 -0800

    Greetings Generator FRPified

--- 1103-th/GreetingsGenerator-demo/GreetingsGenerator-demo/ViewController.swift
+++ 1103-th/GreetingsGenerator-demo/GreetingsGenerator-demo/ViewController.swift
@@ -8,27 +8,78 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-  
-  @IBOutlet weak var stateSegmentedControl: UISegmentedControl!
-  @IBOutlet weak var greetingsLabel: UILabel!
-  @IBOutlet weak var greetingsTextField: UITextField!
-  @IBOutlet weak var nameTextField: UITextField!
-  @IBOutlet var greetingButtons: [UIButton]!
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  @IBAction func stateChanged(_ sender: UISegmentedControl) {
-
-  }
-  
-  @IBAction func greetingsButtonPressed(_ sender: UIButton) {
-
-  }
-  
+    // Variables
+    var buttonText: String = "Hi"
+    let disposeBag = DisposeBag()
+    
+    // UI Elements
+    @IBOutlet weak var stateSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var greetingsLabel: UILabel!
+    @IBOutlet weak var greetingsTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet var greetingButtons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Segmented Observable
+        let segmentedObservable: Observable<Int> = stateSegmentedControl.rx.value.map{ (value: Int) in
+            
+            switch value {
+            case 0:
+                for button in self.greetingButtons {
+                    button.isEnabled = true
+                }
+                self.greetingsTextField.isEnabled = false
+            case 1:
+                for button in self.greetingButtons {
+                    button.isEnabled = false
+                }
+                self.greetingsTextField.isEnabled = true
+                break
+            default:
+                break
+            }
+            
+            return value
+        }
+        
+        // Name Field
+        let nameFieldObservable: Observable<String> = nameTextField.rx.text.map{String($0 ?? "")}
+        
+        // Greeting Text Field
+        let greetingTextFieldObservable: Observable<String> = greetingsTextField.rx.text.map{String($0 ?? "")}
+        
+        // Buttons observable
+        let greetingTextButtonObservable: Observable<String> = buttonText
+        
+        // Combined greeting
+        let combinedGreetingObservable: Observable<String> = Observable.combineLatest(segmentedObservable, nameFieldObservable, greetingTextButtonObservable, greetingTextFieldObservable) { (segmentedIndex: Int, nameField: String, greetingButton: String, greetingField: String) in
+            switch segmentedIndex {
+            case 0: return greetingButton + ", " + nameField
+            case 1: return greetingField + ", " + nameField
+            default: return ""
+            }
+        }
+        combinedGreetingObservable
+            .bindTo(greetingsLabel.rx.text)
+            .addDisposableTo(disposeBag)
+        
+    }
+    
+    @IBAction func stateChanged(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func greetingsButtonPressed(_ sender: UIButton) {
+        self.buttonText = sender.titleLabel?.text ?? ""
+    }
+    
 }
 
 
